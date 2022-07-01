baf1c00c18639496481dc0ded118ed43b4a68e61
Chloe <chloe.pr@pm.me>
2022-07-02 01:40:17 +0200

    make backend url a environment variable

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,14 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getSwitches() {
-    return this.http.get<Object[]>('http://127.0.0.1:8000/api/switches/?format=json');
+    return this.http.get<Object[]>(`${environment.backend_url}/api/switches/?format=json`);
   }
 
   getRooms() {
-    return this.http.get<Object[]>('http://127.0.0.1:8000/api/rooms/?format=json');
+    return this.http.get<Object[]>(`${environment.backend_url}/api/rooms/?format=json`);
   }
 
   switchSwitch(name: any, state: any) {
-    return this.http.put(`http://127.0.0.1:8000/api/switches/${name}/`, {"state": state});
+    return this.http.put(`${environment.backend_url}/api/switches/${name}/`, {"state": state});
   }
 }
--- src/app/titlebar/titlebar.component.ts
+++ src/app/titlebar/titlebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-titlebar',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./titlebar.component.scss']
 })
 export class TitlebarComponent implements OnInit {
+
+  admin_site_url = `${environment.backend_url}/admin/`;
 
   constructor() { }
 
